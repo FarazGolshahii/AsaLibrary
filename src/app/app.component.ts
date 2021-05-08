be38d42c70180c06e9f056ca06650f0c3f4d2b1d
Faraz <faraz.golshahi8151@gmail.com>
2021-05-08 13:05:11 +0430

    add Chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Chart } from 'chart.js';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent{
   isCollapsed = false;
+  title="choose a date";
+  minDate: object = {};
+
+  constructor() {}
+  public pieChartLabels = ['Sales Q1', 'Sales Q2','Sales Q5', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90,88];
+  public pieChartType = 'pie';
+  ngOnInit() {  
+    this.minDate = moment('1395-11-22', 'jYYYY,jMM,jDD');
+  };
+  datePickerConfig = {
+    drops: 'down',
+    opens:'right',
+    format: 'YY/MM/DD',
+    onOpenDelay:'0',
+}
 }
